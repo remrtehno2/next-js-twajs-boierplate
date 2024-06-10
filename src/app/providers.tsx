c882bc169d5d409bc13a831836ce1d21bb393d34
Igor Kuryakov <igor.kuryakov@hpe.com>
2024-06-10 13:52:53 +0500

    Add Suspense

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,14 @@
 import { NextUIProvider } from "@nextui-org/system";
+import { Suspense } from "react";
 
 export interface ProvidersProps {
   children: React.ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+    </NextUIProvider>
+  );
 }
